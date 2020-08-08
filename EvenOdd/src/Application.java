d869dfdc4ac921c747ea196cb9534772a2b3ada7
sachinbansal789 <sachinbansal789@gmail.com>
2020-08-08 19:20:32 +0530

    Removal of checkEven() method

--- EvenOdd/src/Application.java
+++ EvenOdd/src/Application.java
@@ -2,20 +2,20 @@ import java.util.Scanner;
 
 public class Application {
 
-    public boolean checkEven(int num){
-        if (num % 2 == 0){
-            return true;
-        }else{
-            return false;
-        }
-    }
+//    public boolean checkEven(int num){
+//        if (num % 2 == 0){
+//            return true;
+//        }else{
+//            return false;
+//        }
+//    }
 
     public static void main(String[] args) {
-        Application app = new Application();
+//        Application app = new Application();
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number : ");
         int num = sc.nextInt();
-        boolean result = app.checkEven(num);
+//        boolean result = app.checkEven(num);
         if (num % 2 == 0){
             System.out.println("Given number is Even");
         }else{

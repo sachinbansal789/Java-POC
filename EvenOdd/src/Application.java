9f04aadba0abf1df8a3ee8e564dc83f49bfdfd0d
sachinbansal789 <sachinbansal789@gmail.com>
2020-08-08 19:13:28 +0530

    Initial Commit

---
+++ EvenOdd/src/Application.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Application {
+
+    public boolean checkEven(int num){
+        if (num % 2 == 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        Application app = new Application();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number : ");
+        int num = sc.nextInt();
+        boolean result = app.checkEven(num);
+        if (num % 2 == 0){
+            System.out.println("Given number is Even");
+        }else{
+            System.out.println("Given number is Odd");
+        }
+    }
+}
